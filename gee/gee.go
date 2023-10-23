2a66d57f3c2f01fb52a4c97786e0b077aa5810fc
yung <yjz@yungs-MacBook-Pro.local>
2023-10-23 14:38:16 +0800

    add context day 2

--- gee/gee.go
+++ gee/gee.go
@@ -1,23 +1,21 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandleFunc func(w http.ResponseWriter, r *http.Request)
+type HandleFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandleFunc
+	router *router
 }
 
 func New() *Engine {
-	return &Engine{router: make(map[string]HandleFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandleFunc){
-	key := method + "-" + pattern
-	engine.router[key] = handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandleFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
 
 func (engine *Engine) GET(pattern string, handler HandleFunc) {
@@ -28,16 +26,11 @@ func (engine *Engine) POST(pattern string, handler HandleFunc) {
 	engine.addRoute("POST", pattern, handler)
 }
 
-
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 not FOUND %s \n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
